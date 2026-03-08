58854fcf62a47f7acaa9baebcd86200a4c11042d
mr zou <a.tshiyanze@gmail.com>
2026-03-08 15:09:43 +0100

    creation et get de categorie

--- src/main/kotlin/emy/backend/lawapp50/app/contenus/application/service/CategorieService.kt
+++ src/main/kotlin/emy/backend/lawapp50/app/contenus/application/service/CategorieService.kt
@@ -1,6 +1,21 @@
 package emy.backend.lawapp50.app.contenus.application.service
 
+import emy.backend.lawapp50.app.contenus.infrastructure.persistance.entity.CategorieEntity
+import emy.backend.lawapp50.app.contenus.infrastructure.persistance.repository.CategorieRepository
+import kotlinx.coroutines.flow.Flow
 import org.springframework.stereotype.Service
 
 @Service
-class CategorieService {}
+class CategorieService(private val r: CategorieRepository) {
+    suspend fun create(c: CategorieEntity): CategorieEntity{
+        return r.save(c)
+    }
+
+    suspend fun findById(id:Long): CategorieEntity?{
+        return r.findById(id)
+    }
+
+    suspend fun getAll():Flow<CategorieEntity>{
+        return r.findAll()
+    }
+}
--- src/main/kotlin/emy/backend/lawapp50/app/contenus/infrastructure/controller/CategorieController.kt
+++ src/main/kotlin/emy/backend/lawapp50/app/contenus/infrastructure/controller/CategorieController.kt
@@ -1,6 +1,85 @@
 package emy.backend.lawapp50.app.contenus.infrastructure.controller
 
-import org.springframework.web.bind.annotation.RestController
+import emy.backend.lawapp50.app.contenus.application.service.CategorieService
+import emy.backend.lawapp50.app.contenus.domain.model.CategorieRequest
+import emy.backend.lawapp50.app.contenus.infrastructure.persistance.entity.CategorieEntity
+import emy.backend.lawapp50.route.contenu.CategorieScope
+import emy.backend.lawapp50.security.monitoring.MetricModel
+import emy.backend.lawapp50.security.monitoring.SentryService
+import io.swagger.v3.oas.annotations.Operation
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.validation.Valid
+import kotlinx.coroutines.coroutineScope
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
 
 @RestController
-class CategorieController {}
+class CategorieController (
+    private val s: CategorieService,
+    private val sentry : SentryService
+) {
+    @Operation(summary = "Creation de categorie")
+    @PostMapping("/{version}/${CategorieScope.PROTECTED}",produces = [MediaType.APPLICATION_JSON_VALUE])
+    suspend fun createCategorie(
+        @Valid @RequestBody rData: CategorieRequest, req: HttpServletRequest
+    ) = coroutineScope {
+        val startNanos = System.nanoTime()
+        try {
+            val data = CategorieEntity(
+                name = rData.name,
+                isActive = true
+            )
+
+            val createCategorie = s.create(data)
+            mapOf("contenu" to createCategorie)
+        } finally {
+            sentry.callToMetric(
+                MetricModel(
+                    startNanos = startNanos,
+                    status = "200",
+                    route = "${req.method} /${req.requestURI}",
+                    countName = "api.categorie.createcategorie.count",
+                    distributionName = "api.categorie.createcategorie.latency"
+                )
+            )
+        }
+    }
+
+    @Operation(summary = "recuperation des categories contenu")
+    @GetMapping("/{version}/${CategorieScope.PROTECTED}",produces = [MediaType.APPLICATION_JSON_VALUE])
+    suspend fun getAllCategorie(req: HttpServletRequest) = coroutineScope {
+        val startNanos = System.nanoTime()
+        try {
+            mapOf("accounts" to s.getAll())
+        } finally {
+            sentry.callToMetric(
+                MetricModel(
+                    startNanos = startNanos,
+                    status = "200",
+                    route = "${req.method} /${req.requestURI}",
+                    countName = "api.categorie.getallcategorie.count",
+                    distributionName = "api.categorie.getallcategorie.latency"
+                )
+            )
+        }
+    }
+
+    @Operation(summary = "recuperer une categorie contenu par id")
+    @GetMapping("/{version}/${CategorieScope.PROTECTED}/{id}",produces = [MediaType.APPLICATION_JSON_VALUE])
+    suspend fun getById(req: HttpServletRequest, @PathVariable id: Long) = coroutineScope {
+        val startNanos = System.nanoTime()
+        try {
+            mapOf("accounts" to s.findById(id))
+        } finally {
+            sentry.callToMetric(
+                MetricModel(
+                    startNanos = startNanos,
+                    status = "200",
+                    route = "${req.method} /${req.requestURI}",
+                    countName = "api.categorie.getaonecategorie.count",
+                    distributionName = "api.categorie.getaonecategorie.latency"
+                )
+            )
+        }
+    }
+}
