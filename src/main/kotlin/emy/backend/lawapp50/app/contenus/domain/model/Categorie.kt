c53b5fd45df03576ca4913791d1b5ae9455fe92a
mr zou <a.tshiyanze@gmail.com>
2026-03-08 15:09:43 +0100

    creation et get de categorie

--- src/main/kotlin/emy/backend/lawapp50/app/contenus/domain/model/Categorie.kt
+++ src/main/kotlin/emy/backend/lawapp50/app/contenus/domain/model/Categorie.kt
@@ -1,1 +1,14 @@
 package emy.backend.lawapp50.app.contenus.domain.model
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+
+data class Categorie(
+    val id: Long? = null,
+    var name: String,
+    var isActive: Boolean = false,
+)
+
+data class CategorieRequest(
+    var name: String
+)
