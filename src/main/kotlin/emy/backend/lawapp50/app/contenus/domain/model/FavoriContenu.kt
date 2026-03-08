87523e25af0e5b42a4b4f97dacd3750015d40d6d
mr zou <a.tshiyanze@gmail.com>
2026-03-08 16:52:39 +0100

    favori creation sans restructions

--- src/main/kotlin/emy/backend/lawapp50/app/contenus/domain/model/FavoriContenu.kt
+++ src/main/kotlin/emy/backend/lawapp50/app/contenus/domain/model/FavoriContenu.kt
@@ -1,1 +1,22 @@
 package emy.backend.lawapp50.app.contenus.domain.model
+
+import jakarta.validation.constraints.NotNull
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+import java.time.LocalDateTime
+
+data class FavorisContenu(
+    val id: Long? = null,
+    var contenuId: Long,
+    var userId: Long,
+    var favorite: Boolean = true,
+    var isActive: Boolean = true,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+)
+
+data class FavorisContenuRequest(
+    @NotNull
+    var contenuId: Long,
+    @NotNull
+    var userId: Long
+)
--- src/main/kotlin/emy/backend/lawapp50/app/contenus/infrastructure/controller/FavoriContenuController.kt
+++ src/main/kotlin/emy/backend/lawapp50/app/contenus/infrastructure/controller/FavoriContenuController.kt
@@ -1,6 +1,91 @@
 package emy.backend.lawapp50.app.contenus.infrastructure.controller
 
-import org.springframework.web.bind.annotation.RestController
+import emy.backend.lawapp50.app.contenus.application.service.FavoriContenuService
+import emy.backend.lawapp50.app.contenus.domain.model.FavorisContenuRequest
+import emy.backend.lawapp50.app.contenus.infrastructure.persistance.entity.FavorisContenusEntity
+import emy.backend.lawapp50.app.user.application.service.UserService
+import emy.backend.lawapp50.route.contenu.FavoriScope
+import emy.backend.lawapp50.security.monitoring.MetricModel
+import emy.backend.lawapp50.security.monitoring.SentryService
+import io.swagger.v3.oas.annotations.Operation
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.validation.Valid
+import kotlinx.coroutines.coroutineScope
+import org.springframework.context.annotation.Profile
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
 
 @RestController
-class FavoriContenuController {}
+@RequestMapping
+@Profile("dev")
+class FavoriContenuController(
+    private val s: FavoriContenuService,
+    private  val sentry: SentryService,
+    private val userS: UserService,
+) {
+    @Operation(summary = "Creation de favoris")
+    @PostMapping("/{version}/${FavoriScope.PROTECTED}",produces = [MediaType.APPLICATION_JSON_VALUE])
+    suspend fun createFavoris(
+        @Valid @RequestBody rData: FavorisContenuRequest, req: HttpServletRequest
+    ) = coroutineScope {
+        val startNanos = System.nanoTime()
+        try {
+            val user = userS.findIdUser(rData.userId) // ?: ResponseEntity.badRequest().body(mapOf("errer" to "User non trouvr"))
+            val data = FavorisContenusEntity(
+                contenuId = rData.contenuId,
+                userId = rData.userId,
+                favorite = true,
+                isActive = true,
+            )
+
+            val createfavoris = s.create(data)
+            mapOf("contenu" to createfavoris)
+        } finally {
+            sentry.callToMetric(
+                MetricModel(
+                    startNanos = startNanos,
+                    status = "200",
+                    route = "${req.method} /${req.requestURI}",
+                    countName = "api.favoris.createfavoris.count",
+                    distributionName = "api.favoris.createfavoris.latency"
+                )
+            )
+        }
+    }
+
+    @GetMapping("/{version}/${FavoriScope.PROTECTED}",produces = [MediaType.APPLICATION_JSON_VALUE])
+    suspend fun getAllFavoris(req: HttpServletRequest) = coroutineScope {
+        val startNanos = System.nanoTime()
+        try {
+            mapOf("accounts" to s.getAll())
+        } finally {
+            sentry.callToMetric(
+                MetricModel(
+                    startNanos = startNanos,
+                    status = "200",
+                    route = "${req.method} /${req.requestURI}",
+                    countName = "api.favoris.getallfavoris.count",
+                    distributionName = "api.favoris.getallfavoris.latency"
+                )
+            )
+        }
+    }
+
+    @GetMapping("/{version}/${FavoriScope.PROTECTED}/{id}",produces = [MediaType.APPLICATION_JSON_VALUE])
+    suspend fun getById(req: HttpServletRequest, @PathVariable id: Long) = coroutineScope {
+        val startNanos = System.nanoTime()
+        try {
+            mapOf("accounts" to s.findById(id))
+        } finally {
+            sentry.callToMetric(
+                MetricModel(
+                    startNanos = startNanos,
+                    status = "200",
+                    route = "${req.method} /${req.requestURI}",
+                    countName = "api.favoris.getallfavoris.count",
+                    distributionName = "api.favoris.getallfavoris.latency"
+                )
+            )
+        }
+    }
+}
