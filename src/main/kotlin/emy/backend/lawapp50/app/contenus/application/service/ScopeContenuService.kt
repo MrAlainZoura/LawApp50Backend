2b5370db07cd7a399fd2140b7a7e292afbbbf403
mr zou <a.tshiyanze@gmail.com>
2026-03-17 15:39:25 +0100

    update retourn contenu with scope and type

--- src/main/kotlin/emy/backend/lawapp50/app/contenus/application/service/ScopeContenuService.kt
+++ src/main/kotlin/emy/backend/lawapp50/app/contenus/application/service/ScopeContenuService.kt
@@ -1,5 +1,6 @@
 package emy.backend.lawapp50.app.contenus.application.service
 
+import emy.backend.lawapp50.app.contenus.domain.model.ScopeContenuDTO
 import emy.backend.lawapp50.app.contenus.infrastructure.persistance.entity.ScopeContenuEntity
 import emy.backend.lawapp50.app.contenus.infrastructure.persistance.repository.ScopeContenuRepository
 import kotlinx.coroutines.flow.firstOrNull
@@ -8,7 +9,10 @@ import kotlinx.coroutines.flow.toList
 import org.springframework.stereotype.Service
 
 @Service
-class ScopeContenuService(private val r: ScopeContenuRepository) {
+class ScopeContenuService(
+    private val r: ScopeContenuRepository,
+    private val scopS: ScopeService
+) {
     suspend fun create(c: ScopeContenuEntity): ScopeContenuEntity{
         return r.save(c)
     }
@@ -16,11 +20,19 @@ class ScopeContenuService(private val r: ScopeContenuRepository) {
     suspend fun findById(id:Long): ScopeContenuEntity?{
         return r.findById(id)
     }
-//    suspend fun getFavoriteIfExist(contenuId: Long, user: Long): ScopeContenuEntity? {
-//        val like: ScopeContenuEntity? = r.findFavoriteExist(contenuId, user)?.firstOrNull()
-//        return like
-//    }
+    suspend fun getScopeContenu(contenuId: Long): List<ScopeContenuDTO>? {
+        val scop = r.findScopeContenu(contenuId)?.map { toDtoEntity(it) }?.toList()
+        return scop
+    }
     suspend fun getAll(): List<ScopeContenuEntity>{
         return r.findAll().map{it}.toList()
+    }
+
+    suspend fun toDtoEntity(it: ScopeContenuEntity): ScopeContenuDTO {
+        return ScopeContenuDTO(
+            scope = scopS.findById(it.scopeId),
+            contenu = null,
+            scopeContenu = it
+        )
     }
 }
