98ea289d6a9e7d00ac2fb1050c2ec54f1c8b8980
ElieOko <elieoko100@gmail.com>
2026-04-03 16:04:30 +0100

    feat(app:evaluation->reposiroty):nouvelle requete dans la source de donnée

--- src/main/kotlin/emy/backend/lawapp50/app/evaluation/infrastructure/persistance/repository/EvaluationRepository.kt
+++ src/main/kotlin/emy/backend/lawapp50/app/evaluation/infrastructure/persistance/repository/EvaluationRepository.kt
@@ -1,6 +1,17 @@
 package emy.backend.lawapp50.app.evaluation.infrastructure.persistance.repository
 
 import emy.backend.lawapp50.app.evaluation.infrastructure.persistance.entity.EvaluationEntity
+import kotlinx.coroutines.flow.Flow
+import org.springframework.data.r2dbc.repository.Modifying
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
-interface EvaluationRepository  : CoroutineCrudRepository<EvaluationEntity, Long>{}
+interface EvaluationRepository  : CoroutineCrudRepository<EvaluationEntity, Long>{
+
+    @Query("SELECT * FROM evaluations WHERE user_id = :userId")
+    suspend fun getAllByUser(userId: Long) : Flow<EvaluationEntity?>
+
+    @Modifying
+    @Query("""UPDATE evaluations SET is_active = :state WHERE user_id = :userId""")
+    suspend fun setUpdateIsAvailable(userId: Long, state: Boolean = false): Int
+}
